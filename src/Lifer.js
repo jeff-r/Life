49b0d2c6f8bb93014b67aa93920d007117a43ec6
Jeff Roush <jeff@jeffroush.com>
2011-07-21 14:43:53 -0700

    Fix a layout bug.

--- src/Lifer.js
+++ src/Lifer.js
@@ -1,11 +1,33 @@
 function LifeDoc(lifediv) {
   this.lifeGrid = new LifeGrid(lifediv);
-  this.topdiv = lifediv;
-  this.width = 200;      // width in pixels
+  this.topdiv   = lifediv;
+  this.longestDimension    = 300;      // width in pixels
+
+  this.cellWidth  = 10;
+  this.cellHeight = 10;
 }
 
 
 function startLife() {
+  var glider = [ 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
   var blinker = [ 
     [ 0, 0, 0, 0, 0], 
     [ 0, 0, 1, 0, 0], 
@@ -20,13 +42,14 @@ function startLife() {
     var lifediv = document.getElementById("life_outer");
 
     var lifedoc = new LifeDoc(lifediv);
-    lifedoc.addArray(blinker);
-    lifedoc.makeGridElements();
+    lifedoc.addArray(glider);
+    lifedoc.makeTable();
+    lifedoc.setGridColors();
     document.lifeDoc = lifedoc;
     document.lifeHasStarted = true;
   }
 
-  var timer = setTimeout("startLife()", 3000);
+  var timer = setTimeout("startLife()", 500);
 }
 
 LifeDoc.prototype.runOneCycle = function() {
@@ -35,7 +58,7 @@ LifeDoc.prototype.runOneCycle = function() {
 }
 
 LifeDoc.prototype.cellsAsFlattenedArray = function() {
-  var allrows = this.topdiv.getElementsByTagName("div");
+  var allrows = this.topdiv.getElementsByTagName("td");
   var cells = new Array();
   var cellnum = 0;
   for (n=0; n<allrows.length; n++)
@@ -45,6 +68,18 @@ LifeDoc.prototype.cellsAsFlattenedArray = function() {
   this.cells = cells;
   return cells;
 }
+
+//LifeDoc.prototype.cellsAsFlattenedArray = function() {
+//  var allrows = this.topdiv.getElementsByTagName("div");
+//  var cells = new Array();
+//  var cellnum = 0;
+//  for (n=0; n<allrows.length; n++)
+//    if (allrows[n].className == "deadcell" || allrows[n].className == "livecell")
+//      cells[cellnum++] = allrows[n];
+//
+//  this.cells = cells;
+//  return cells;
+//}
 
 LifeDoc.prototype.cellAt = function(x,y) {
   var index = y*this.lifeGrid.numCols() + x;
@@ -63,16 +98,26 @@ LifeDoc.prototype.setGridColors = function(array) {
       if (1 == this.lifeGrid.valueAt(x,y))
         className = "livecell";
 
-      // console.log("cell("+x+","+y+"): " + this.cellAt(x,y).className);
       this.cellAt(x,y).className = className;
+      // if (className == "livecell")
+        // console.log("cell("+x+","+y+"): " + this.cellAt(x,y).className);
     }
   }
 }
 
 LifeDoc.prototype.addArray = function(array) {
   this.lifeGrid   = new LifeGrid(array);
-  this.cellWidth  = this.width / this.lifeGrid.numCols();
-  this.cellHeight = this.cellWidth;
+
+  // this.cellWidth  = this.longestDimension / this.lifeGrid.numCols();
+  // this.cellHeight = this.longestDimension / this.lifeGrid.numRows();
+  // min = Math.min (this.cellWidth, this.cellHeight);
+
+  // console.log("cellHeight = " + this.cellHeight);
+  // console.log("cellWidth = " + this.cellWidth);
+  // console.log("min = " + min);
+  // this.cellWidth  = min;
+  // this.cellHeight = min;
+  
 }
 
 LifeDoc.prototype.addDiv = function(parentElement, className, width, height) {
@@ -91,26 +136,38 @@ LifeDoc.prototype.addDiv = function(parentElement, className, width, height) {
   return div;
 }
 
-LifeDoc.prototype.makeGridElements = function() {
+LifeDoc.prototype.makeTable = function() {
   var rows = this.lifeGrid.numRows();
   var cols = this.lifeGrid.numCols();
-  var rowwidth = cols * (this.cellWidth + 2);
+  var rowwidth = cols * (this.cellWidth + 1);
 
-  for (x=0; x<cols; x++) {
-    var rowDiv = this.addDiv(this.topdiv, "rowdiv", rowwidth);
-    for (y=0; y<rows; y++) {
+  // console.log("rows = " + rows + "; cols = " + cols);
+  // console.log("cellWidth = " + this.cellWidth);
+
+  var table = document.createElement("table");
+
+  for (y=0; y<rows; y++) {
+    var tr = document.createElement("tr");
+    tr.style.width = rowwidth + "px";
+    for (x=0; x<cols; x++) {
       var className = "deadcell";
       if (1 == this.lifeGrid.valueAt(x,y))
         className = "livecell";
-      this.addDiv(rowDiv, className, this.cellWidth, this.cellHeight);
+      var td = document.createElement("td");
+      td.className    = className;
+      td.style.width  = this.cellWidth  + "px";
+      td.style.height = this.cellHeight + "px";
+      tr.appendChild(td);
     }
+    table.appendChild(tr);
   }
 
-  var height = rows * (this.cellHeight + 1) + 1;
+  this.topdiv.appendChild(table);
+
   var width  = cols * (this.cellWidth  + 1) + 1;
+  // console.log("width = " + width);
+  var height = rows * (this.cellHeight + 1) + 1;
   this.topdiv.style.height = height + "px";
   this.topdiv.style.width = width + "px";
 }
-
-
 
